591a7119d33bff0b25838198dd5af34c23590ae9
Schneidoa <daniel@schneidoa.de>
2016-06-14 21:55:42 +0200

    initial commit

--- src/EasyAnnotationClass.php
+++ src/EasyAnnotationClass.php
@@ -10,12 +10,7 @@ namespace Schneidoa\EasyAnnotation;
 
 class EasyAnnotationClass extends \ReflectionClass
 {
-
-    public function __construct($argument)
-    {
-        parent::__construct($argument);
-    }
-
+    
     public function getProperty($name)
     {
         return new EasyAnnotationProperty($this->getName(), $name);
